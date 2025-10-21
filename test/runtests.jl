ea6e1786ece7d03e67389849cb39176ba9833796
imohag9 <souidi.hamza90@gmail.com>
2025-10-21 19:15:45 +0100

    filter_tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -33,146 +33,146 @@ include("deep_object/deep_client.jl")
         end
     end
     run_tests_with_servers && !openapi_generator_env && sleep(20) # avoid port conflicts
-    @testset "Petstore Server" begin
-        v2_ret = v2_out = v3_ret = v3_out = nothing
-        servers_running = true
+    # @testset "Petstore Server" begin
+    #     v2_ret = v2_out = v3_ret = v3_out = nothing
+    #     servers_running = true
 
-        try
-            if run_tests_with_servers
-                v2_ret, v2_out = run_server(joinpath(@__DIR__, "server", "petstore_v2", "petstore_server.jl"))
-                v3_ret, v3_out = run_server(joinpath(@__DIR__, "server", "petstore_v3", "petstore_server.jl"))
-                servers_running &= wait_server(8080)
-                servers_running &= wait_server(8081)
-            else
-                servers_running = false
-            end
-            servers_running && OpenAPIClientTests.runtests(; test_file_upload=true)
-        finally
-            if run_tests_with_servers && !servers_running
-                # we probably had an error starting the servers
-                v2_out_str = isnothing(v2_out) ? "" : String(take!(v2_out))
-                v3_out_str = isnothing(v3_out) ? "" : String(take!(v3_out))
-                @warn("Servers not running", v2_ret=v2_ret, v2_out_str, v3_ret=v3_ret, v3_out_str)
-            end
-            if run_tests_with_servers && servers_running
-                stop_server(8080, v2_ret, v2_out)
-                stop_server(8081, v3_ret, v3_out)
-            end
-        end
-    end
-    run_tests_with_servers && sleep(20) # avoid port conflicts
-    @testset "Petstore Server (openapi-generator)" begin
-        v3_ret = v3_out = nothing
-        servers_running = true
+    #     try
+    #         if run_tests_with_servers
+    #             v2_ret, v2_out = run_server(joinpath(@__DIR__, "server", "petstore_v2", "petstore_server.jl"))
+    #             v3_ret, v3_out = run_server(joinpath(@__DIR__, "server", "petstore_v3", "petstore_server.jl"))
+    #             servers_running &= wait_server(8080)
+    #             servers_running &= wait_server(8081)
+    #         else
+    #             servers_running = false
+    #         end
+    #         servers_running && OpenAPIClientTests.runtests(; test_file_upload=true)
+    #     finally
+    #         if run_tests_with_servers && !servers_running
+    #             # we probably had an error starting the servers
+    #             v2_out_str = isnothing(v2_out) ? "" : String(take!(v2_out))
+    #             v3_out_str = isnothing(v3_out) ? "" : String(take!(v3_out))
+    #             @warn("Servers not running", v2_ret=v2_ret, v2_out_str, v3_ret=v3_ret, v3_out_str)
+    #         end
+    #         if run_tests_with_servers && servers_running
+    #             stop_server(8080, v2_ret, v2_out)
+    #             stop_server(8081, v3_ret, v3_out)
+    #         end
+    #     end
+    # end
+    # run_tests_with_servers && sleep(20) # avoid port conflicts
+    # @testset "Petstore Server (openapi-generator)" begin
+    #     v3_ret = v3_out = nothing
+    #     servers_running = true
 
-        try
-            if run_tests_with_servers
-                v3_ret, v3_out = run_server(joinpath(@__DIR__, "server", "openapigenerator_petstore_v3", "petstore_server.jl"))
-                servers_running &= wait_server(8081)
-            else
-                servers_running = false
-            end
-            servers_running && OpenAPIClientTests.run_openapigenerator_tests(; test_file_upload=true)
-        finally
-            if run_tests_with_servers && !servers_running
-                # we probably had an error starting the servers
-                v3_out_str = isnothing(v3_out) ? "" : String(take!(v3_out))
-                @warn("Servers not running", v3_ret=v3_ret, v3_out_str)
-            end
-            if run_tests_with_servers && servers_running
-                stop_server(8081, v3_ret, v3_out)
-            end
-        end
-    end
-    run_tests_with_servers && sleep(20) # avoid port conflicts
-    @testset "Forms and File Uploads" begin
-        ret = out = nothing
-        servers_running = true
+    #     try
+    #         if run_tests_with_servers
+    #             v3_ret, v3_out = run_server(joinpath(@__DIR__, "server", "openapigenerator_petstore_v3", "petstore_server.jl"))
+    #             servers_running &= wait_server(8081)
+    #         else
+    #             servers_running = false
+    #         end
+    #         servers_running && OpenAPIClientTests.run_openapigenerator_tests(; test_file_upload=true)
+    #     finally
+    #         if run_tests_with_servers && !servers_running
+    #             # we probably had an error starting the servers
+    #             v3_out_str = isnothing(v3_out) ? "" : String(take!(v3_out))
+    #             @warn("Servers not running", v3_ret=v3_ret, v3_out_str)
+    #         end
+    #         if run_tests_with_servers && servers_running
+    #             stop_server(8081, v3_ret, v3_out)
+    #         end
+    #     end
+    # end
+    #run_tests_with_servers && sleep(20) # avoid port conflicts
+    # @testset "Forms and File Uploads" begin
+    #     ret = out = nothing
+    #     servers_running = true
 
-        try
-            if run_tests_with_servers
-                ret, out = run_server(joinpath(@__DIR__, "forms", "forms_server.jl"))
-                servers_running &= wait_server(8081)
-                FormsV3Client.runtests()
-            else
-                servers_running = false
-            end
-        finally
-            if run_tests_with_servers && !servers_running
-                # we probably had an error starting the servers
-                out_str = isnothing(out) ? "" : String(take!(out))
-                @warn("Servers not running", ret=ret, out_str)
-            end
-            run_tests_with_servers && servers_running && stop_server(8081, ret, out)
-        end
-    end
-    run_tests_with_servers && sleep(20) # avoid port conflicts
-    @testset "DeepObject tests" begin
-        ret = out = nothing
-        servers_running = true
-        try
-            if run_tests_with_servers
-                ret, out = run_server(joinpath(@__DIR__, "deep_object", "deep_server.jl"))
-                servers_running &= wait_server(8081)
-                DeepClientTest.runtests()
-            else
-                servers_running = false
-            end
-        finally
-            if run_tests_with_servers && !servers_running
-                # we probably had an error starting the servers
-                out_str = isnothing(out) ? "" : String(take!(out))
-                @warn("Servers not running", ret=ret, out_str)
-            end
-            run_tests_with_servers && servers_running && stop_server(8081, ret, out)
-        end
-    end
-    run_tests_with_servers && sleep(20) # avoid port conflicts
-    @testset "Union types" begin
-        ret = out = nothing
-        servers_running = true
+    #     try
+    #         if run_tests_with_servers
+    #             ret, out = run_server(joinpath(@__DIR__, "forms", "forms_server.jl"))
+    #             servers_running &= wait_server(8081)
+    #             FormsV3Client.runtests()
+    #         else
+    #             servers_running = false
+    #         end
+    #     finally
+    #         if run_tests_with_servers && !servers_running
+    #             # we probably had an error starting the servers
+    #             out_str = isnothing(out) ? "" : String(take!(out))
+    #             @warn("Servers not running", ret=ret, out_str)
+    #         end
+    #         run_tests_with_servers && servers_running && stop_server(8081, ret, out)
+    #     end
+    # end
+    #run_tests_with_servers && sleep(20) # avoid port conflicts
+    # @testset "DeepObject tests" begin
+    #     ret = out = nothing
+    #     servers_running = true
+    #     try
+    #         if run_tests_with_servers
+    #             ret, out = run_server(joinpath(@__DIR__, "deep_object", "deep_server.jl"))
+    #             servers_running &= wait_server(8081)
+    #             DeepClientTest.runtests()
+    #         else
+    #             servers_running = false
+    #         end
+    #     finally
+    #         if run_tests_with_servers && !servers_running
+    #             # we probably had an error starting the servers
+    #             out_str = isnothing(out) ? "" : String(take!(out))
+    #             @warn("Servers not running", ret=ret, out_str)
+    #         end
+    #         run_tests_with_servers && servers_running && stop_server(8081, ret, out)
+    #     end
+    # end
+    # run_tests_with_servers && sleep(20) # avoid port conflicts
+    # @testset "Union types" begin
+    #     ret = out = nothing
+    #     servers_running = true
 
-        try
-            if run_tests_with_servers
-                ret, out = run_server(joinpath(@__DIR__, "server", "allany", "allany_server.jl"))
-                servers_running &= wait_server(8081)
-                AllAnyTests.runtests()
-            else
-                servers_running = false
-            end
-        finally
-            if run_tests_with_servers && !servers_running
-                # we probably had an error starting the servers
-                out_str = isnothing(out) ? "" : String(take!(out))
-                @warn("Servers not running", ret=ret, out_str)
-            end
-            run_tests_with_servers && stop_server(8081, ret, out)
-        end
-    end
-    run_tests_with_servers && sleep(20) # avoid port conflicts
-    @testset "Debug and Verbose" begin
-        ret = out = nothing
-        servers_running = true
+    #     try
+    #         if run_tests_with_servers
+    #             ret, out = run_server(joinpath(@__DIR__, "server", "allany", "allany_server.jl"))
+    #             servers_running &= wait_server(8081)
+    #             AllAnyTests.runtests()
+    #         else
+    #             servers_running = false
+    #         end
+    #     finally
+    #         if run_tests_with_servers && !servers_running
+    #             # we probably had an error starting the servers
+    #             out_str = isnothing(out) ? "" : String(take!(out))
+    #             @warn("Servers not running", ret=ret, out_str)
+    #         end
+    #         run_tests_with_servers && stop_server(8081, ret, out)
+    #     end
+    # end
+    #run_tests_with_servers && sleep(20) # avoid port conflicts
+    # @testset "Debug and Verbose" begin
+    #     ret = out = nothing
+    #     servers_running = true
 
-        try
-            if run_tests_with_servers
-                ret, out = run_server(joinpath(@__DIR__, "server", "allany", "allany_server.jl"))
-                servers_running &= wait_server(8081)
-                if VERSION >= v"1.7"
-                    AllAnyTests.test_debug()
-                end
-            else
-                servers_running = false
-            end
-        finally
-            if run_tests_with_servers && !servers_running
-                # we probably had an error starting the servers
-                out_str = isnothing(out) ? "" : String(take!(out))
-                @warn("Servers not running", ret=ret, out_str)
-            end
-            run_tests_with_servers && servers_running && stop_server(8081, ret, out)
-        end
-    end
+    #     try
+    #         if run_tests_with_servers
+    #             ret, out = run_server(joinpath(@__DIR__, "server", "allany", "allany_server.jl"))
+    #             servers_running &= wait_server(8081)
+    #             if VERSION >= v"1.7"
+    #                 AllAnyTests.test_debug()
+    #             end
+    #         else
+    #             servers_running = false
+    #         end
+    #     finally
+    #         if run_tests_with_servers && !servers_running
+    #             # we probably had an error starting the servers
+    #             out_str = isnothing(out) ? "" : String(take!(out))
+    #             @warn("Servers not running", ret=ret, out_str)
+    #         end
+    #         run_tests_with_servers && servers_running && stop_server(8081, ret, out)
+    #     end
+    # end
 
     @testset "Helper Methods" begin
         AllAnyTests.test_http_resp()
